108ef3bfe621b395c65797e8a36a5ead7b43214f
WS01Dev <wsdev@null.dev>
2021-01-30 13:15:52 +0700

    SPH RLE: allow skip specified false positive bits

--- sph_rle_experimental.py
+++ sph_rle_experimental.py
@@ -2,6 +2,14 @@ import struct
 import os
 import sys
 
+if len(sys.argv) < 3:
+	print(f"Not enough arguments! usage: {sys.argv[0]} file output [skip_bit_offsets]", file=sys.stderr)
+	sys.exit(1)
+
+skip_bits = []
+if len(sys.argv) > 3:
+	skip_bits = [int(k, 16) for k in sys.argv[3].split(",")]
+
 sz = os.path.getsize(sys.argv[1])
 fd = open(sys.argv[1], "rb")
 
@@ -10,7 +18,7 @@ obuf = bytearray()
 
 while fd.tell() < sz:
 	rl_offs = struct.unpack("<H", fd.read(2))[0]
-	if rl_offs == 1:
+	if rl_offs == 1 and fd.tell()-2 not in skip_bits:
 		bit = fd.read(2)
 		cnt = struct.unpack("<H", fd.read(2))[0]
 		obuf += bit*cnt
