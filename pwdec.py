b18f9d8f9458b2ff1925fd45f5b6d9cadb4ca6a4
WS01Dev <wsdev@null.dev>
2021-05-26 15:03:19 +0700

    improved sanyo RLE decoding

--- pwdec.py
+++ pwdec.py
@@ -7,7 +7,7 @@ sz = os.path.getsize(sys.argv[1])
 df = open(sys.argv[1], "rb")
 pts = []
 
-assert df.read(3) == b"PW\x00" # SI\x10\x00
+assert df.read(3) == b"PW\x00" # PW\x00
 palette_size = df.read(1)[0]+1
 width = df.read(1)[0]
 height = df.read(1)[0]
@@ -28,13 +28,32 @@ for _ in range(palette_size):
 	pts.append(rgb565toi24(df.read(2)))
 
 temp = bytearray()
+raw = False
+pixel = 0
 
 while df.tell()<sz:
 	cnt = df.read(1)[0]
-	#print(cnt)
-	if cnt in [0xff,0x0]: continue
-	p_bit = df.read(1)[0]	
-	adt = pts[p_bit]
+	if cnt == 0xff: 
+		raw = False
+		pixel = 0
+		continue
+	if cnt == 0x00 and (not raw or pixel >= width): 
+		raw = True
+		pixel = 0
+		continue
+
+	if raw:
+		if cnt > len(pts): 
+			print(f"wrong palette {cnt}")
+			cnt = 0
+		pixel += 1
+		temp += pts[cnt]
+		continue
+	cpl = df.read(1)[0]
+	if cpl > len(pts): 
+		print(f"wrong palette {cpl}")
+		cpl = 0
+	adt = pts[cpl]
 	temp += adt*cnt
 	
 Image.frombuffer("RGB", (width, height), bytes(temp)).save(sys.argv[2])
--- rawsanyorledec.py
+++ rawsanyorledec.py
@@ -1,10 +1,12 @@
 import os
 import sys
+import struct
 
 sz = os.path.getsize(sys.argv[1])
 df = open(sys.argv[1], "rb")
 of = open(sys.argv[2], "wb")
 plte = open(sys.argv[3], "rb")
+width = int(sys.argv[4])
 
 pts = []
 b = True
@@ -15,8 +17,43 @@ while b != b"":
 	pts.append(b)
 	b = plte.read(2)
 
+raw = False
+#raw2 = False
+cnto = 0
+
 while df.tell()<sz:
+	#print("b",hex(df.tell()))
 	cnt = df.read(1)[0]
-	if cnt in [0xff,0x0]: continue
-	adt = pts[df.read(1)[0]]
+	if cnt == 0xff: 
+		raw = False
+		cnto = 0
+		#raw2 = False
+		continue
+	if cnt == 0x00 and (not raw or cnto >= width): 
+		#of.write(pts[cnt])
+		#df.read(1)
+		raw = True
+		cnto = 0
+		continue
+	'''
+	elif cnt == 0x00 and raw and not raw2:
+		raw2 = True
+		continue
+	elif cnt != 0x00 and raw2:
+		raw2 = False
+	'''
+	if raw:
+		of.write(pts[cnt])
+		cnto += 1
+		#df.read(1)
+		continue
+	cpl = df.read(1)[0]
+	if cpl > len(pts): 
+		#print("perror")
+		continue
+	adt = pts[cpl]
+	#print("a",hex(of.tell()))
+	#if cpl == 0xc and cnt == 0xc:
+		#of.write(adt*2)
+	#lse:
 	of.write(adt*cnt)
