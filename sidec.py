6a944083fea5b73358303cb2c6fa2a8d55410783
WS01Dev <wsdev@null.dev>
2021-05-26 15:12:44 +0700

    a

--- sidec.py
+++ sidec.py
@@ -30,13 +30,32 @@ for _ in range(palette_size):
 	pts.append(rgb565toi24(df.read(2)))
 
 temp = bytearray()
+raw = False
+pixel = 0
 
 while df.tell()<sz:
 	cnt = df.read(1)[0]
-	#print(cnt)
-	if cnt in [0xff,0x0]: continue
-	p_bit = df.read(1)[0]	
-	adt = pts[p_bit]
+	if cnt == 0xff: 
+		raw = False
+		pixel = 0
+		continue
+	if cnt == 0x00 and (not raw or pixel >= width): 
+		raw = True
+		pixel = 0
+		continue
+
+	if raw:
+		if cnt > len(pts): 
+			print(f"wrong palette {cnt}")
+			cnt = 0
+		pixel += 1
+		temp += pts[cnt]
+		continue
+	cpl = df.read(1)[0]
+	if cpl > len(pts): 
+		print(f"wrong palette {cpl}")
+		cpl = 0
+	adt = pts[cpl]
 	temp += adt*cnt
 	
 Image.frombuffer("RGB", (width, height), bytes(temp)).save(sys.argv[2])
